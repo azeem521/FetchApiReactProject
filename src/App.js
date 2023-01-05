c7145e30ecc87d4d67ec4001647e8106d9257bc5
Mohd <azeemsre1@gmail.com>
2023-01-05 13:48:03 +0530

    UsingAsyncAwait

--- src/App.js
+++ src/App.js
@@ -5,25 +5,11 @@ import './App.css';
 
 function App() {
   const [movie,setMovie]=useState([])
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
+  
 
-  const fetchMoviesHandler=()=>{
-    fetch('https://swapi.dev/api/films/').then((response)=>{
-      return response.json()
-    }).then((data)=>{
+  async function fetchMoviesHandler(){
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data=await response.json();
       const transformedMovies=data.results.map((movieData)=>{
         return {
           id:movieData.episode_id,
@@ -33,7 +19,6 @@ function App() {
         }
       });
       setMovie(transformedMovies);
-    })
   }
 
   return (
